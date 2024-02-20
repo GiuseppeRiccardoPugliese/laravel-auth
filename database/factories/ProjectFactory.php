e2b551d72907b0fbe055f08ec0d5d99d3d89be00
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-20 17:53:13 +0100

    Factory

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(3, false),
+            'image' => fake()->imageUrl(360, 360, 'animals', true, 'cats'),
+        ];
+    }
+}
