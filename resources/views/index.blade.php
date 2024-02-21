84bf37099d593ebc90106fb66c478b6d04e0cf48
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-02-21 13:04:53 +0100

    feat: style card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="d-flex justify-content-center align-items-center">
-        <h1 class="text-danger fw-bolder">Projects: {{ count($projects) }}</h1>
+        <h1 class="text-danger fw-bolder">Our Projects: {{ count($projects) }}</h1>
 
         {{-- @auth --}}
 
@@ -17,7 +17,7 @@
             @foreach ($projects as $project)
                 <div class="col-4 mb-3 d-flex justify-content-center align-items-center">
 
-                    <div class="card" style="width: 18rem; min-height: 250px;">
+                    <div class="card" style="width: 18rem; min-height: 300px;">
                         <img class="card-img-top" src="{{ $project->image }}" alt="Card image cap">
                         <div class="card-body">
                             {{-- Rotta per la SHOW --}}
@@ -27,14 +27,16 @@
                             </a>
                             </h5>
                             {{-- <p class="card-text">{{ $project->description }}</p> --}}
-                            <form method="POST" action="{{ route('project.destroy', $project->id) }}">
-                                {{-- (action)Rotta per la DESTROY --}}
-                                @csrf
-                                @method('DELETE')
-                                <input type="submit" class="mx-2 btn btn-danger" value="DELETE">
-                            </form>
-                            <a class="text-decoration-none" href="{{ route('project.edit', $project->id) }}"><button
-                                    type="button" class="btn btn-info ms-2">EDIT</button></a>
+                            <div class="d-flex">
+                                <form method="POST" action="{{ route('project.destroy', $project->id) }}">
+                                    {{-- (action)Rotta per la DESTROY --}}
+                                    @csrf
+                                    @method('DELETE')
+                                    <input type="submit" class="mx-2 btn btn-danger" value="DELETE">
+                                </form>
+                                <a class="text-decoration-none" href="{{ route('project.edit', $project->id) }}"><button
+                                        type="button" class="btn btn-info ms-2">EDIT</button></a>
+                            </div>
                         </div>
                     </div>
 
