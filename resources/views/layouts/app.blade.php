46b56f54e276a937434e2f2542eb5124db932a2b
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-21 17:30:34 +0100

    Add image, routes autorizzate se loggati, pagina di presentazione se non loggati

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,11 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    {{-- CDN BOOTSTRAP JS  --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 </head>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 //Utilizziamo il main controller
 use App\Http\Controllers\Guest\PageController;
 
+//Utilizziamo il controller per la pagina iniziale se non loggato :D
+use App\Http\Controllers\Guest\HelloUser;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,26 +20,9 @@ use App\Http\Controllers\Guest\PageController;
 |
 */
 
-//Rotta per l'INDEX del controller
-Route::get('/', [PageController::class, 'index'])->name('project.index');
+//Rotta iniziale PRE-LOGIN
+Route::get('/hello', [HelloUser::class, 'index'])->name('hello.user');
 
-//Rotta per la CREATE (in GET)
-Route::get('/project/create', [PageController::class, 'create'])->name('project.create');
-
-//Rotta per la CREATE (in POST)
-Route::post('/project', [PageController::class, 'store'])->name('project.store');
-
-//Rotta per l'EDIT
-Route::get('/project/{id}/edit', [PageController::class, 'edit'])->name('project.edit');
-
-//Rotta per l'UPDATE
-Route::put('/project/{id}', [PageController::class, 'update'])->name('project.update');
-
-//Rotta per la DELETE
-Route::delete('/project/{id}', [PageController::class, 'destroy'])->name('project.destroy');
-
-//Rotta per la SHOW
-Route::get('/project/{id}', [PageController::class, 'show'])->name('project.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -50,6 +36,27 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     //Our Routes
+
+    //Rotta per l'INDEX del controller
+    Route::get('/', [PageController::class, 'index'])->name('project.index');
+
+    //Rotta per la CREATE (in GET)
+    Route::get('/project/create', [PageController::class, 'create'])->name('project.create');
+
+    //Rotta per la CREATE (in POST)
+    Route::post('/project', [PageController::class, 'store'])->name('project.store');
+
+    //Rotta per l'EDIT
+    Route::get('/project/{id}/edit', [PageController::class, 'edit'])->name('project.edit');
+
+    //Rotta per l'UPDATE
+    Route::put('/project/{id}', [PageController::class, 'update'])->name('project.update');
+
+    //Rotta per la DELETE
+    Route::delete('/project/{id}', [PageController::class, 'destroy'])->name('project.destroy');
+
+    //Rotta per la SHOW
+    Route::get('/project/{id}', [PageController::class, 'show'])->name('project.show');
 });
 
 require __DIR__ . '/auth.php';
