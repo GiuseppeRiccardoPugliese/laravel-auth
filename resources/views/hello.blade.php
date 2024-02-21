72630540f7cdae518cea752fb2e1dac56bf35cad
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-21 18:25:33 +0100

    Aggiunta slider per la pagina iniziale di presentazione da non loggato

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -2,34 +2,59 @@
 @section('content')
     <h1 class="text-danger text-center fw-bolder">Our Team!!🤡</h1>
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="{{ asset('mimmo.jpg') }}" alt="First slide">
+    <div id="carouselExampleCaptions" class="carousel slide m-auto">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"
+                aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner h-100  rounded-4">
+            <div class="carousel-item h-100 active">
+                <img src="{{ asset('mimmo.jpg') }}" class="d-block w-100 h-100 object-fit-cover rounded-4" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Domenico Lella</h5>
+                    <p>Mimmucc pe frat fidat</p>
+                </div>
             </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="{{ asset('jack.jpg') }}" alt="Second slide">
+            <div class="carousel-item h-100">
+                <img src="{{ asset('jack.jpg') }}" class="d-block w-100 h-100 object-fit-cover rounded-4" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Giacomo Agave</h5>
+                    <p>Ciao, sono Jack! Ho un superpotere segreto: riesco a far ridere chiunque. Se avete bisogno di una
+                        dose di buon umore, sono qui per farvi sorridere e aggiungere un po' di allegria alla vostra
+                        giornata!</p>
+                </div>
             </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="{{ asset('peppino.jpg') }}" alt="Third slide">
+            <div class="carousel-item h-100">
+                <img src="{{ asset('peppino.jpg') }}" class="d-block w-100 h-100 object-fit-cover rounded-4" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Giuseppe Riccardo Pugliese</h5>
+                    <p>Ciao a tutti, sono Peppe aChuckArell, un napoletano doc con il cuore in pizzeria e la testa sempre
+                        allegra. Sì, sono quello che potrebbe mangiare pizza a colazione, pranzo e cena senza stancarsi mai.
+                        Vi avviso, se mi fate una pizza con l'ananas, non rispondo delle mie azioni!
+                    </p>
+                </div>
             </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="{{ asset('andrew.jpg') }}" alt="Fourth slide">
+            <div class="carousel-item h-100">
+                <img src="{{ asset('andrew.jpg') }}" class="d-block w-100 h-100 object-fit-cover rounded-4" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Andrea Manigrasso</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,9 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     {{-- CDN BOOTSTRAP JS  --}}
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js"
+        integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 </head>
